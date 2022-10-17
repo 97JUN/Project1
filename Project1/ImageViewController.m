e00057b0d2cd4821e1bf16573f8b47be307a3162
97JUN <leehyunjun123440@gmail.com>
2022-10-17 18:49:31 +0900

    project 수정

--- Project1/ImageViewController.m
+++ Project1/ImageViewController.m
@@ -20,13 +20,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [MySwitch setOn: false];
-    [MySwitch setEnabled:NO];
-    [MyIndicator startAnimating];
-    [self.MyImage setImage:[UIImage imageNamed:@"ic_control_call-mic_off"]];
+//    [MySwitch setEnabled:NO];
+//    [MyIndicator startAnimating];
+//
     
     
-    [self performSelector:@selector(ImageMake) withObject:nil afterDelay:2.0];
+  //  [self performSelector:@selector(ImageMake) withObject:nil afterDelay:2.0];
     
+    //제스터 등록 방법
+    //1. xib에서 컴포넌트 사용하듯이 추가해주는 방법
+    //2. 코드 상에서 추가해주는 방법
+    //View 등록, View에 gesture 등록
+    
+    //xib에서 컴포넌트 사용하듯이 추가
     
     
   //  [self startTimer];
@@ -38,28 +44,40 @@
     
     
 }
+- (IBAction)tapGesture:(id)sender {
+    [MyIndicator stopAnimating];
+    
+    
+    [self performSelector:@selector(ImageMake) withObject:nil afterDelay:2.0];
+    
+}
 
-//-(void)startTimer {
-    
-    
-    //timerCount = 1;
-    
-    
-  //  timerRepeat = [NSTimer scheduledTimerWithTimeInterval: 1.0 target:self selector:@selector(timerFire:) userInfo:nil repeats:false];
-    
-//}
+- (void)onGesture{
+    //Gesture가 들어오는 곳
+    //인디케이터 시작
+    // performSelector (딜레이 2초)
+}
 
-//- (void)timerFireMethod:(NSTimer *)timer {
-//
-//}
+
 
 
 -(void)ImageMake {
-    [MySwitch setOn:true];
-    [MySwitch setEnabled:YES];
-    [self.MyImage setImage:[UIImage imageNamed:@"40"]];
     [MyIndicator stopAnimating];
-    self.MyIndicator.hidden = YES;
+    
+    if (MySwitch.isOn == true){
+        [self.MyImage setImage:[UIImage imageNamed:@"40"]];
+    }else {
+        [self.MyImage setImage:[UIImage imageNamed:@"ic_control_call-mic_off"]];
+    }
+    
+    
+    //인디케이터 stop
+    //switch 상태에 따라서 함수 구현
+//    [MySwitch setOn:true];
+//    [MySwitch setEnabled:YES];
+//    [self.MyImage setImage:[UIImage imageNamed:@"40"]];
+//    [MyIndicator stopAnimating];
+//    self.MyIndicator.hidden = YES;
 }
 
 
