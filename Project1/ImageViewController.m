45b54a18d81ca311f3ee3ddb65647358dc8aecf2
Hyunjun Lee <90228137+97JUN@users.noreply.github.com>
2022-10-18 01:23:42 +0900

    gesture recognizer 기능구현

--- Project1/ImageViewController.m
+++ Project1/ImageViewController.m
@@ -20,6 +20,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [MySwitch setOn: false];
+    [self.MyImage setImage:[UIImage imageNamed:@"40"]];
+    
 //    [MySwitch setEnabled:NO];
 //    [MyIndicator startAnimating];
 //
@@ -45,18 +47,21 @@
     
 }
 - (IBAction)tapGesture:(id)sender {
-    [MyIndicator stopAnimating];
+    
+    [MyIndicator startAnimating];
     
     
     [self performSelector:@selector(ImageMake) withObject:nil afterDelay:2.0];
     
+    
+    
 }
 
-- (void)onGesture{
-    //Gesture가 들어오는 곳
-    //인디케이터 시작
-    // performSelector (딜레이 2초)
-}
+//- (void)onGesture{
+//    //Gesture가 들어오는 곳
+//    //인디케이터 시작
+//    // performSelector (딜레이 2초)
+//}
 
 
 
@@ -66,8 +71,11 @@
     
     if (MySwitch.isOn == true){
         [self.MyImage setImage:[UIImage imageNamed:@"40"]];
-    }else {
+        [MySwitch setOn:NO];
+        
+    }else if (MySwitch.isOn == false) {
         [self.MyImage setImage:[UIImage imageNamed:@"ic_control_call-mic_off"]];
+        [MySwitch setOn:YES];
     }
     
     
